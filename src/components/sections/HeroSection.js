0429b921c71fdc07815ef88a090d8b28e777937d
hxsseeb <hasseeb473@gmail.com>
2020-12-27 15:03:40 +0000

    Completed episode 10

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import PurchaseButton from "../buttons/PurchaseButton"
+import MockupAnimation from "../animations/MockupAnimation"
 
 export default function HeroSection() {
   return (
@@ -20,6 +21,7 @@ export default function HeroSection() {
           </Description>
           <PurchaseButton title="Purchase now" subtitle="£19 per month" />
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   )
@@ -27,12 +29,15 @@ export default function HeroSection() {
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
-  padding: 200px 30px;
   margin: 0 auto;
+  padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `
 
 const TextWrapper = styled.div`
