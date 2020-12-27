b9f99c2e75947dcee04f2af130a74a0b250c7297
hxsseeb <hasseeb473@gmail.com>
2020-12-27 17:50:23 +0000

    Completed episode 12

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -2,5 +2,5 @@ export const menuData = [
   { title: "Features", icon: "/images/icons/courses.svg", link: "/features" },
   { title: "Partners", icon: "/images/icons/tutorials.svg", link: "/partners" },
   { title: "Contact", icon: "/images/icons/pricing.svg", link: "/contact" },
-  { title: "Dashboard", icon: "/images/icons/account.svg", link: "/dashboard" },
+  { title: "", icon: "/images/icons/account.svg", link: "/dashboard" },
 ]
