1467a30e7464a6c9d6380202002ab74dc054183e
hxsseeb <hasseeb473@gmail.com>
2021-03-07 17:54:19 +0000

    Reset to episode 16

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
-import FeatureSection from "../components/sections/FeatureSection"
 import HeroSection from "../components/sections/HeroSection"
 
 function IndexPage() {
@@ -9,7 +8,6 @@ function IndexPage() {
     <Layout>
       <SEO title="Home" />
       <HeroSection />
-      <FeatureSection />
     </Layout>
   )
 }
